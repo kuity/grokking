406cffe9db7d8fc0ae09d0af01f04566aec1d619
kuity <jlz777@gmail.com>
2022-11-15 21:49:53 +0800

    sync

--- src/k_way_merge/largest_pairs.cpp
+++ src/k_way_merge/largest_pairs.cpp
@@ -2,6 +2,7 @@ using namespace std;
 
 #include <iostream>
 #include <vector>
+#include <queue>
 
 class LargestPairs {
  public:
@@ -11,8 +12,53 @@ class LargestPairs {
   static vector<pair<int, int>> findKLargestPairs(const vector<int> &nums1,
                                                   const vector<int> &nums2, int k) {
     vector<pair<int, int>> result;
+    vector<int> buf_max;
+    vector<int> buf_next;
+    bool last_is_one;
+    int i = 0;
+    int j = 0;
+    int counter = 0;
+    if (nums1[0] >= nums2[0]) {
+      last_is_one = true;
+      buf_next.push_back(nums1[0]);
+      i++;
+    } else {
+      last_is_one = false;
+      buf_next.push_back(nums2[0]);
+      j++;
+    }
 
+    while(i<nums1.size() && j<nums2.size() && counter < k) {
+      auto l1_num = nums1[i];
+      auto l2_num = nums2[j];
+      int greatest;
+      bool current_is_one;
+      if (nums1[i] >= nums2[j]) {
+        greatest = nums1[i];
+        i++;
+        current_is_one = true;
+      } else {
+        greatest = nums2[j];
+        j++;
+        current_is_one = false;
+      } 
 
+      // If there is a "flip" i.e. this max element belongs to different list as last max element
+      if (current_is_one != last_is_one) {
+        buf_max = buf_next;
+        buf_next = {};
+        last_is_one = !last_is_one;
+      }
+
+      for (auto x: buf_max) {
+        result.push_back(make_pair(x, greatest));
+        buf_next.push_back(greatest);
+        counter++;
+        if (counter == k) {
+          break;
+        }
+      }
+    }
 
     return result;
   }
@@ -25,3 +71,10 @@ int main(int argc, char *argv[]) {
     cout << "[" << pair.first << ", " << pair.second << "] ";
   }
 }
+
+
+x x x     x
+      y y
+
+x x x   x
+      y   y
